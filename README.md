672a52ca2f48123a6d4c983919900d7c443d69cd
Bryan Hou <boyuhou@gmail.com>
2018-01-10 19:17:57 -0500

    all weekly charts

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # curr-research
 Currency Research
+
---
+++ rename_files.py
@@ -0,0 +1,26 @@
+import os
+import shutil
+
+TICKERS = ['AUDCAD', 'AUDCHF', 'AUDJPY', 'AUDNZD', 'AUDUSD', 'CADCHF', 'CADJPY', 'CHFJPY', 'EURAUD', 'EURCAD', 'EURCHF',
+           'EURGBP', 'EURJPY', 'EURNZD', 'EURUSD', 'GBPAUD', 'GBPCAD', 'GBPCHF', 'GBPJPY', 'GBPNZD', 'GBPUSD', 'NZDCAD',
+           'NZDCHF', 'NZDJPY', 'NZDUSD', 'USDCAD', 'USDCHF', 'USDJPY']
+
+
+def process_file(processing_folder):
+    for d in [_dir for _dir in os.listdir(processing_folder) if os.path.isdir(_dir)]:
+        if str.isnumeric(d):
+            rename_files(processing_folder, d)
+
+
+def rename_files(processing_folder, sub_dir):
+    dir = os.path.join(processing_folder, sub_dir)
+    dir_files = os.listdir(dir)
+    dir_files.sort()
+    for i in range(0, len(dir_files)):
+        old_file = os.path.join(dir, dir_files[i])
+        new_file = os.path.join(processing_folder, 'weekly', '{0}_{1}.png'.format(TICKERS[i], sub_dir))
+        shutil.move(old_file, new_file)
+
+
+if __name__ == '__main__':
+    process_file('.')
